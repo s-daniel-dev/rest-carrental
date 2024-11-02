5c16623f2e65f833319778af15358e62b46dee95
s-daniel-dev <stemler.daniel@gmail.com>
2024-11-02 10:43:59 +0100

    Push to origin

---
+++ Sb_CarRent_REST/src/main/java/SD/Sb_CarRent_REST/SbCarRentRestApplication.java
@@ -0,0 +1,13 @@
+package SD.Sb_CarRent_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCarRentRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCarRentRestApplication.class, args);
+	}
+
+}
---
+++ Sb_CarRent_REST/src/test/java/SD/Sb_CarRent_REST/SbCarRentRestApplicationTests.java
@@ -0,0 +1,13 @@
+package SD.Sb_CarRent_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCarRentRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
